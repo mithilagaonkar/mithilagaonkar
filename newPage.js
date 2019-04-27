dbf2b2e708963a72cde3dce0840aec6839777fd6
Priyesh <priyesh.dkr@gmail.com>
2019-04-27 21:29:27 +0530

    new Apis and ui (#4)
    add new ui and delete user api

--- newPage.js
+++ newPage.js
@@ -1,6 +1,12 @@
 function getUserListing() {
 
-    getUsers();
+    var users = getUsers();
+    document.getElementById('userList').append(users.toString());
     console.log('get a list of users');
     
+};
+
+function deleteUser(id) {
+    deleteUser(id);
+    console.log('get a list of roles for users...');
 };
