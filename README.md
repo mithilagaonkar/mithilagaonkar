67ea073be4ba1a21a88b91c6e184e51b1345f035
Mithila Gaonkar <mithila_gaonkar@wifi-soft.com>
2019-04-27 19:53:05 +0530

    Added the needed changes

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Hi MIthila, this is development.
 Adding apis in development.
 
 Adding UI for APIs.
+
+Added css framework along with bootstrap.
--- newfile.js
+++ newfile.js
@@ -1,3 +1,5 @@
 console.log('HI...adding apis.');
 
 console.log('Hi....I am adding the UI files...');
+
+console.log("Hey...This is wrt UI....Making it more responsive....");
