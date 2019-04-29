8b4e0daf924be6116c31b4d11471b2ed5ee4788b
Mithila Gaonkar <mithila_gaonkar@wifi-soft.com>
2019-04-29 21:08:25 +0530

    Added new branch learning

--- README.md
+++
@@ -1,7 +0,0 @@
-Hi MIthila, this is development.
-
-Adding apis in development.
-
-Adding UI for APIs.
-
-Added css framework along with bootstrap.
--- newfile.js
+++
@@ -1,5 +0,0 @@
-console.log('HI...adding apis.');
-
-console.log('Hi....I am adding the UI files...');
-
-console.log("Hey...This is wrt UI....Making it more responsive....");
